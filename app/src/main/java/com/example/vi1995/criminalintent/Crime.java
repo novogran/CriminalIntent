62158a03bbc1458b9b063f9dd9082d5cdd3d1944
Vitaly Novogran <vi19954@gmail.com>
2018-01-13 03:42:55 +0300

    Initial commit

---
+++ app/src/main/java/com/example/vi1995/criminalintent/Crime.java
@@ -0,0 +1,25 @@
+package com.example.vi1995.criminalintent;
+
+import java.util.UUID;
+
+public class Crime {
+
+    private UUID mId;
+    private String mTitle;
+
+    public Crime(){
+        mId = UUID.randomUUID();
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+}
